6181219e793fc013ef8df169e896da9ecf847e68
Lauzaki <L@L.local>
2021-08-13 16:20:14 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://linktr.ee/lauzaki"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Socials
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
